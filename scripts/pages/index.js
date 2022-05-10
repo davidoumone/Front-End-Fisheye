b05377a02519c0192bf7b230d07a1539eac6ac4b
Davidoumone <david.moneta28@gmail.com>
2022-05-11 01:21:38 +0200

    fetch requete modifs

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,11 +1,11 @@
 async function getPhotographers() {
   //  les données récupérées dans le json
   try {
-    let response = await fetch("../../data/photographers.json");
-    // let response = await fetch(
-    //   "https://github.com/davidoumone/Front-End-Fisheye/blob/main/data/photographers.json",
-    //   { mode: "no-cors" }
-    // );
+    // let response = await fetch("../../data/photographers.json");
+    let response = await fetch(
+      "https://github.com/davidoumone/Front-End-Fisheye/blob/main/data/photographers.json",
+      { mode: "no-cors" }
+    );
 
     if (response.ok) {
       // si le statut HTTP est 200-299
